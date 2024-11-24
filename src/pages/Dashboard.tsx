166caecb60260714cc4406f710a3d90426b028c9
iamrohitg <rohitghosh76@gmail.com>
2024-11-24 20:16:48 +0530

    Add components: Add tailwind and schadcn for css and components

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 function Home() {
@@ -5,6 +6,7 @@ function Home() {
     <div>
       <h2>Home Page</h2>
       <p>Welcome to the home page!</p>
+      <Button>button</Button>
     </div>
   );
 }
